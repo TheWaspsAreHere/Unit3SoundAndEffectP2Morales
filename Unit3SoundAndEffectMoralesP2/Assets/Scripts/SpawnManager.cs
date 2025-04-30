3efb85f563e12b3de32cf26f7ef4da52cfa3e800
unicornglitterpoop <719045@student.dusd.net>
2025-04-30 09:56:11 -0700

    new scripts

--- Unit3SoundAndEffectMoralesP2/Assets/Scripts/SpawnManager.cs
+++ Unit3SoundAndEffectMoralesP2/Assets/Scripts/SpawnManager.cs
@@ -2,28 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveLeft : MonoBehaviour
+public class SpawnManager : MonoBehaviour
 {
-    private float speed = 30;
+    public GameObject obstaclePrefab;
+    private Vector3 spawnPos = new Vector3(25, 0, 0);
+    private float startDelay = 2;
+    private float repeatRate = 2;
     private PlayerController playerControllerScript;
-    private float leftBound;
 
     // Start is called before the first frame update
     void Start()
     {
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
         playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    void SpawnObstacle ()
+    {
         if (playerControllerScript.gameOver == false)
         {
-            transform.Translate(Vector3.left * Time.deltaTime * speed);
-        }
-        if (transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
-        {
-            Destroy(gameObject);
+            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
         }
     }
 }
