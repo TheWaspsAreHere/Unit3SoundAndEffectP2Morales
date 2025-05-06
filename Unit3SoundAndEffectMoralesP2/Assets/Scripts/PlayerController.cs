4ca04db0ce8fca965440f7866315a2ef0e5d893a
unicornglitterpoop <719045@student.dusd.net>
2025-05-06 09:57:40 -0700

    update

--- Unit3SoundAndEffectMoralesP2/Assets/Scripts/PlayerController.cs
+++ Unit3SoundAndEffectMoralesP2/Assets/Scripts/PlayerController.cs
@@ -7,12 +7,15 @@ public class PlayerController : MonoBehaviour
 
 {
     private Rigidbody playerRb;
-    public float jumpForce = 10;
+    public float jumpForce;
     public float gravityModifier;
     public bool isOnGround = true;
     public bool gameOver;
     public ParticleSystem explosionParticle;
     public ParticleSystem dirtParticle;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+    private AudioSource playerAudio;
 
     private Animator playerAnim;
 
@@ -22,6 +25,7 @@ public class PlayerController : MonoBehaviour
         playerRb = GetComponent<Rigidbody>();
         playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
+        playerAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -33,6 +37,7 @@ public class PlayerController : MonoBehaviour
             isOnGround = false;
             playerAnim.SetTrigger("Jump_trig");
             dirtParticle.Stop();
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
         }
     }
 
@@ -52,6 +57,7 @@ public class PlayerController : MonoBehaviour
             playerAnim.SetInteger("DeathType_int", 1);
             explosionParticle.Play();
             dirtParticle.Stop();
+            playerAudio.PlayOneShot(crashSound, 1.0f);
         }
     }
 }
